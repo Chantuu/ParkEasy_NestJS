d27cfbcb2ca69244275a2f9e5b7e552df8ce6e15
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 15:56:11 +0400

    Add TypeORM integration to API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'main.db',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
