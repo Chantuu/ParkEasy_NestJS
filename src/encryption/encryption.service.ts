0d1b33a8698cd91977cc951c448fe90922f7b406
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-02-01 01:22:41 +0400

    Implement Encryption Service Functionality
      Implemented encrypt and decrypt functionality in this service, which will be sharable across different modules.

--- src/encryption/encryption.service.ts
+++ src/encryption/encryption.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { randomBytes, createCipheriv, createDecipheriv } from 'crypto';
 
 @Injectable()
-export class EncryptionService {}
+export class EncryptionService {
+  private readonly algorithm: string;
+  private readonly key: string;
+
+  constructor(private _configService: ConfigService) {
+    this.algorithm = this._configService.get<string>('ENCRYPTION_ALGORITHM')!;
+    this.key = this._configService.get<string>('ENCRYPTION_KEY')!;
+  }
+
+  encrypt(stringToEncrypt: string): string {
+    const iv = randomBytes(16); // Generate a random IV for every encryption
+    const cipher = createCipheriv(this.algorithm, this.key, iv);
+
+    const encrypted = Buffer.concat([
+      cipher.update(stringToEncrypt, 'utf8'),
+      cipher.final(),
+    ]);
+
+    // Return IV + Encrypted Data as a single hex string
+    return iv.toString('hex') + ':' + encrypted.toString('hex');
+  }
+
+  decrypt(stringToDecrypt: string): string {
+    const [ivHex, dataHex] = stringToDecrypt.split(':'); // Split IV and data
+    const iv = Buffer.from(ivHex, 'hex');
+    const encryptedData = Buffer.from(dataHex, 'hex');
+
+    const decipher = createDecipheriv(this.algorithm, this.key, iv);
+
+    const decrypted = Buffer.concat([
+      decipher.update(encryptedData),
+      decipher.final(),
+    ]);
+
+    return decrypted.toString('utf8');
+  }
+}
