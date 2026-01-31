0d397c474e7b23ebed10e0110156cfb0ada2b757
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 18:56:40 +0400

    Implement Cookie Session in the Application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //#region Cookie Session Setup
+  const configService = app.get<ConfigService>(ConfigService);
+  const sessionSecret = configService.get<string>('SESSION_SECRET'); // Get Cookie Session Secret Key
+
+  if (!sessionSecret) {
+    throw new Error('SESSION_SECRET environment variable is not defined.');
+  }
+  app.use(
+    cookieSession({
+      keys: [sessionSecret], // Set Cookie Session Secret Key
+      maxAge: 24 * 60 * 60 * 1000, // Cookie Session is valid for 24 hours
+    }),
+  );
+  //#endregion
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Removes any properties not specified in DTOs
