b52216c7b551984fea31e6b1428fe1edcf9d2a73
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 18:04:54 +0400

    Implement POST api/auth/register Endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotImplementedException,
+  Post,
+} from '@nestjs/common';
+import { RegisterUserDTO } from './DTOs/register-user.dto';
+import { AuthService } from './auth.service';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(private _authService: AuthService) {}
+
+  @Post('register')
+  async register(@Body() registerUserDTO: RegisterUserDTO) {
+    return await this._authService.register(registerUserDTO);
+  }
+}
