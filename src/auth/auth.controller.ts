1222aca9fba0f8964f776bc1f148038ebbfad3a4
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-01-31 20:25:29 +0400

    Refactor AuthController
     Refactored controller methods to add user information saving to session automatically.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Session } from '@nestjs/common';
 import { RegisterUserDTO } from './DTOs/register-user.dto';
 import { AuthService } from './auth.service';
 import { LoginUserDTO } from './DTOs/login-user.dto';
+import { saveUserToSession } from 'src/helper/functions/save-user-to-session.function';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private _authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() registerUserDTO: RegisterUserDTO) {
+  async register(
+    @Body() registerUserDTO: RegisterUserDTO,
+    @Session() session: Record<string, any>,
+  ) {
     const result = await this._authService.register(registerUserDTO);
+
+    saveUserToSession(result, session);
+
     return {
       success: true,
       message: `Successfully registered user ${result.fullName}`,
@@ -17,8 +24,14 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(@Body() loginUserDTO: LoginUserDTO) {
+  async login(
+    @Body() loginUserDTO: LoginUserDTO,
+    @Session() session: Record<string, any>,
+  ) {
     const result = await this._authService.login(loginUserDTO);
+
+    saveUserToSession(result, session);
+
     return {
       success: true,
       message: `Successfully signed in user ${result.fullName}`,
