e934ad39d84fd6324f9f104f4836fff0818164dc
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-03-12 03:15:19 +0400

    Create Swagger Documentations for Auth Endpoints and DTOs
      Fully wrote detailed documentation for auth controller endpoints and associated DTO objects.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,6 +15,20 @@ import { AuthGuard } from './auth.guard';
 import { CurrentUser } from 'src/helper/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { successResponse } from 'src/helper/functions/success-response.function';
+import {
+  ApiBadRequestResponse,
+  ApiCookieAuth,
+  ApiForbiddenResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { ValidationError } from 'class-validator';
+import {
+  emailAlreadyExistsErrorMessage,
+  unauthorizedErrorMessage,
+  userWithEmailPasswordNotExistsErrorMessage,
+} from 'src/helper/messages/messages.variables';
 
 /**
  * This controller is responsible for handling authentication routes.
@@ -30,6 +44,52 @@ export class AuthController {
    * @param session - Session object.
    * @returns Response object with success and message fields.
    */
+  @ApiOperation({
+    summary: 'Register new user.',
+    description:
+      'Registers new user in the application with request body validated for correct information and returns newly registered user.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Successfully registered new user.',
+    schema: {
+      example: {
+        status: 'success',
+        data: {
+          id: '1aef23df-ac7c-4022-9637-03df7e7374d8',
+          fullName: 'Giorgi Chanturia',
+          email: 'giorgi@email.com',
+          money: 20,
+        },
+        message: 'Successfully registered user Giorgi Chanturia',
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request variations when registering new user.',
+    content: {
+      'application/json': {
+        examples: {
+          validationError: {
+            summary: 'Request body validation errors',
+            value: {
+              statusCode: 400,
+              message: ['email must be an email'],
+              error: 'Bad Request',
+            },
+          },
+          userIsRegisteredError: {
+            summary: 'User with specified email is already registered',
+            value: {
+              message: emailAlreadyExistsErrorMessage,
+              error: 'Bad Request',
+              statusCode: 400,
+            },
+          },
+        },
+      },
+    },
+  })
   @Post('register')
   async register(
     @Body() registerUserDTO: RegisterUserDTO,
@@ -57,6 +117,52 @@ export class AuthController {
    * @param session - Session object.
    * @returns Response object with success and message fields.
    */
+  @ApiOperation({
+    summary: 'Sign in the application.',
+    description:
+      'Sign in the application with request body validated for correct information and returns newly signed in user.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Successfully registered new user.',
+    schema: {
+      example: {
+        status: 'success',
+        data: {
+          id: '1aef23df-ac7c-4022-9637-03df7e7374d8',
+          fullName: 'Giorgi Chanturia',
+          email: 'giorgi@email.com',
+          money: 20,
+        },
+        message: 'Successfully signed in user Giorgi Chanturia',
+      },
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request variations when registering new user.',
+    content: {
+      'application/json': {
+        examples: {
+          validationError: {
+            summary: 'Request body validation errors',
+            value: {
+              statusCode: 400,
+              message: ['email must be an email'],
+              error: 'Bad Request',
+            },
+          },
+          userIsRegisteredError: {
+            summary: 'User does not exist with specified credentials',
+            value: {
+              message: userWithEmailPasswordNotExistsErrorMessage,
+              error: 'Bad Request',
+              statusCode: 400,
+            },
+          },
+        },
+      },
+    },
+  })
   @Post('login')
   async login(
     @Body() loginUserDTO: LoginUserDTO,
@@ -84,6 +190,38 @@ export class AuthController {
    * @param currentUser - Current user obtained by Param decorator.
    * @returns Promise with response object containing current user data
    */
+  @ApiCookieAuth()
+  @ApiOperation({
+    summary: 'Get currently signed in user.',
+    description: 'Returns currently signed in user information as a response.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully retrieved current user.',
+    schema: {
+      example: {
+        status: 'success',
+        data: {
+          id: '1aef23df-ac7c-4022-9637-03df7e7374d8',
+          fullName: 'Giorgi Chanturia',
+          email: 'giorgi@email.com',
+          money: 20,
+        },
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: 'User is not signed in in the application.',
+    content: {
+      'application/json': {
+        example: {
+          statusCode: 400,
+          message: unauthorizedErrorMessage,
+          error: 'Unauthorized',
+        },
+      },
+    },
+  })
   @Get('currentUser')
   @UseGuards(AuthGuard)
   async getCurrentUser(@CurrentUser() currentUser: User) {
@@ -102,6 +240,33 @@ export class AuthController {
    * @param session - Session object.
    * @returns Response object with success and message fields.
    */
+  @ApiCookieAuth()
+  @ApiOperation({
+    summary: 'Log out current user.',
+    description: 'Logs out current user from the application.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully logged out current user.',
+    schema: {
+      example: {
+        status: 'success',
+        message: 'Successfully logged out user Giorgi Chanturia',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: 'User is not signed in in the application.',
+    content: {
+      'application/json': {
+        example: {
+          statusCode: 401,
+          message: unauthorizedErrorMessage,
+          error: 'Unauthorized',
+        },
+      },
+    },
+  })
   @Get('logout')
   @UseGuards(AuthGuard)
   logout(@Session() session: Record<string, any>) {
@@ -124,6 +289,34 @@ export class AuthController {
    * @param session - Session object.
    * @returns Response object with success and message fields.
    */
+  @ApiCookieAuth()
+  @ApiOperation({
+    summary: 'Delete current user.',
+    description:
+      "Completely deletes current user and it's associated data from the application.",
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Successfully logged out current user.',
+    schema: {
+      example: {
+        status: 'success',
+        message: 'Successfully deleted user Giorgi Chanturia',
+      },
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: 'User is not signed in in the application.',
+    content: {
+      'application/json': {
+        example: {
+          statusCode: 401,
+          message: unauthorizedErrorMessage,
+          error: 'Unauthorized',
+        },
+      },
+    },
+  })
   @Delete('delete')
   @UseGuards(AuthGuard)
   async deleteCurrentUser(
