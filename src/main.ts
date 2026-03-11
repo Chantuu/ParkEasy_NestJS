3f03ac025ba17e8404fd6cbe148d707ba2d86f51
Chantuu <47988187+Chantuu@users.noreply.github.com>
2026-03-11 10:58:17 +0400

    Implement Swagger OpenAPI Documentation Endpoint
      Implemented basic swagger documentation, which is available at the /api/docs endpoint.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import cookieSession from 'cookie-session';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -32,6 +33,15 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     credentials: true,
   });
+
+  const openApiConfig = new DocumentBuilder()
+    .setTitle('ParkEasy API')
+    .setDescription('Backend API of the ParkEasy system.')
+    .setVersion('1.0.0')
+    .build();
+  const documentFactory = () =>
+    SwaggerModule.createDocument(app, openApiConfig);
+  SwaggerModule.setup('api/docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
